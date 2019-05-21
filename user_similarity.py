115aabf8acc4806757d75172a34623e78b55cd0f
Geoffrey Li <geoffreyli16@gmail.com>
2019-05-21 15:20:14 -0700

    ready to create cos_sim distance and user graph tables

--- user_similarity.py
+++ user_similarity.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import datetime
 from pyspark import SparkConf, SparkContext
 from google.cloud import bigquery
 from google.oauth2 import service_account
@@ -26,8 +27,7 @@ def connect_db():
     '''
     Connects to datset and returns bigquery client and dataset reference
     '''
-    credentials = service_account.Credentials.from_service_account_file( \
-        'reddit-239517_service_key.json')
+    credentials = service_account.Credentials.from_service_account_file('reddit-239517_service_key.json')
     project_id = 'reddit-239517'
     client = bigquery.Client(credentials= credentials,project=project_id)
     dataset_ref = client.dataset('test1')
@@ -40,51 +40,63 @@ def query_generator(sql_query):
     client, dataset = connect_db()
     query_job = client.query(
         sql_query,
-        location="US",)  # API request - starts the query
-    for row in  query_job:
+        location='US')  # API request - starts the query
+    for row in query_job:
         yield row
+
+def write_to_table(table_name, rows_to_insert):
+    '''
+    Writes the rows to the table
+
+    Arguments:
+        table_name - str, name of table in test1 datset
+        rows_to_insert - list of tuples with number of elements in tuple equal to number of columns in table_name
+    Returns
+        nothing, writes to table
+    '''
+    client, table = get_table(table_name)
+    errors = client.insert_rows(table, rows_to_insert)  # API request
+    assert errors == []
+    return
+
 
 query = (
     "SELECT author, subreddit_id, n_comments "
     "FROM `test1.subredditMembershipv2`"
-    "WHERE author in ('Useless_Patrick','TheScapeQuest')"
+    "WHERE author in ('Useless_Patrick','TheScapeQuest','emsers')"
 )
 
-# API request - starts the query
-# query_job = client.query(
-#     query,
-#     location="US",  # Location must match that of the dataset(s) referenced in the query.
-# )  
+ 
 # API request - fetches results
 # Row values can be accessed by field name or index
+query_job = query_generator(query)
 
 
-# query_job = query_generator(query)
+# Writes QueryJob rows to a list to parallelize into Spark RDD
+query_job_list = list()
+for row in query_job:
+    row = list(row)
+    row.append(1)
+    query_job_list.append(tuple(i for i in row))
 
-# print(type(query_job))
-
-# query_job_list = list()
-
-# for row in query_job:
-#     row = list(row)
-#     row.append(1)
-#     query_job_list.append(tuple(i for i in row))
-
-# # Convert output from QueryJob (list of tuples) into Spark RDD
-# user_sub = sc.parallelize(query_job_list)
+# Convert output from QueryJob (list of tuples) into Spark RDD
+user_sub = sc.parallelize(query_job_list)
 
 # Test RDD Data
-user_sub_count = sc.parallelize([(1, 1), (1, 1), (1, 1), (2, 1), (2, 1), (3, 1), (4, 1), (5, 1)])
-sub_user = sc.parallelize([('a',[2]), ('a',[1]), ('b',[1]), ('c',[3]), ('c',[4]), ('c',[5]), ('c',[1]), ('c',[2])])
-print('sub_user')
-print(sub_user.collect())
-sub_members = sub_user.reduceByKey(lambda a, b: a+b)
-print('sub_members')
-print(sub_members.collect())
-
-# user_sub_count = user_sub.map(lambda x: (x[0], 1))
-# sub_user = user_sub.map(lambda x: (x[1], [x[0]]))
+# user_sub_count = sc.parallelize([(1, 1), (1, 1), (1, 1), (2, 1), (2, 1), (3, 1), (4, 1), (5, 1)])
+# sub_user = sc.parallelize([('a',[2]), ('a',[1]), ('b',[1]), ('c',[3]), ('c',[4]), ('c',[5]), ('c',[1]), ('c',[2])])
+# print('sub_user')
+# print(sub_user.collect())
 # sub_members = sub_user.reduceByKey(lambda a, b: a+b)
+# print('sub_members complete')
+# print(sub_members.collect())
+
+
+user_sub_count = user_sub.map(lambda x: (x[0], 1))
+sub_user = user_sub.map(lambda x: (x[1], [x[0]]))
+sub_members = sub_user.reduceByKey(lambda a, b: a+b)
+print('sub_members complete: '+str(datetime.datetime.now()))
+
 
 # For each subreddit membership list, make tuples of all pairs of users, and map 1 as value
 # The 1 can be replaced as the weighted IDF constant (later)
@@ -96,17 +108,25 @@ user_pairs = sub_members\
 
 # Sort the user-user pairs so they'll group together
 user_pairs = user_pairs.map(lambda x: ((sorted(x[0])[0], sorted(x[0])[1]), x[1]))
-print('user_pairs')
-print(user_pairs.collect())
+print('user_pairs complete: '+str(datetime.datetime.now()))
+# print(user_pairs.collect())
 
 # Number of shared subreddits by user
 # Numerator of the cosine similarity metric (dot product of User1,User2 vectors)
-shared_subs_by_user = user_pairs.reduceByKey(lambda a, b: a+b)
-print('shared_subs_by_user')
-print(shared_subs_by_user.collect())
+shared_subs_by_user_pair = user_pairs.reduceByKey(lambda a, b: a+b)
+write_to_table(shared_subs_user_graph, shared_subs_by_user_pair.collect())
+print('shared_subs_by_user_pair complete: '+str(datetime.datetime.now()))
+# print(shared_subs_by_user_pair.collect())
 
 # Norm of each user's subreddit vector 
 user_norm = user_sub_count.reduceByKey(lambda s1, s2: s1+s2).map(lambda x: (x[0], x[1]**0.5))
-print('user_norm')
-print(user_norm.collect())
+user_norm_lookup = user_norm.collectAsMap()
+print('user_norm complete: '+str(datetime.datetime.now()))
+# print(user_norm.collect())
 
+# Denominator of the cosine similarity distance, norm(user1)*norm(user2), for each user pair
+cos_sim_user_pair = shared_subs_by_user_pair\
+    .map(lambda x: (x[0], 1-x[1]/(user_norm_lookup[x[0][0]]*user_norm_lookup[x[0][1]])))
+write_to_table(cos_sim_user_pair, cos_sim_user_pair.collect())
+print('cos_sim_user_pair complete:'+str(datetime.datetime.now()))
+# print(cos_sim_user_pair.collect())
